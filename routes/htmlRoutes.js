81cc15344ee20fcd4a1bbcd91bcdd827d8d9f092
Groot <ericfinney1@gmail.com>
2019-02-26 20:03:19 -0500

    html routes update feb 26

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,11 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+  // Load Products page
   app.get("/products", function(req, res) {
     db.products
       .findAll({
+        raw: true,
         attributes: [
           "id",
           "sku",
@@ -22,9 +23,10 @@ module.exports = function(app) {
         });
       });
   });
-
+  //Display Cart Contents
   app.get("/cart/:cart_id", function(req, res) {
     db.cart_contents.findAlldb.Cart_Contents.findAll({
+      raw: true,
       attributes: ["id", "cart_id", "quantity"],
       where: {
         cart_id: req.params.cart_id
@@ -45,16 +47,17 @@ module.exports = function(app) {
         }
       ]
     }).then(function(dbCartContents) {
-      res.render("XXXXXXXXXXXX", {
+      res.render("shoppingcart", {
         cart_contents: dbCartContents
       });
     });
   });
 
-  // Load example page and pass in an example by id
+  // Load Product page and pass in the product id
   app.get("/products/:id", function(req, res) {
     db.products
       .findOne({
+        raw: true,
         attributes: [
           "id",
           "sku",
@@ -71,6 +74,33 @@ module.exports = function(app) {
           products: dbProducts
         });
       });
+  });
+
+  // app.post("/api/cart/:cart_id/:product_id", function(req, res) {
+  //   var myCartId, myParam1, myParam2;
+  //   console.log(req.params.cart_id);
+  //   console.log(req.params.product_id);
+  //   myParam1 = parseInt(req.params.cart_id);
+  //   myParam2 = parseInt(req.params.product_id);
+  //   console.log(myParam1);
+  //   console.log(myParam2);
+
+  //   if (myParam1 === 0) {
+  //     db.cart
+  //       .create({
+  //         customer_id: 0
+  //       })
+  //       .then(function(dbCart) {
+  //         myCartId = dbCart.id; //TEST HERE TO SEE IF THIS GIVE YOU THE CART ID!!!
+  //         console.log(myCartId);
+  //         res.json(JSON.stringify(dbCart));
+  //         //db.cart_contents.create()
+  //       });
+  //   }
+
+    // db.Example.create(req.body).then(function(dbExample) {
+    //   res.json(dbExample);
+    // });
   });
 
   // Render 404 page for any unmatched routes
